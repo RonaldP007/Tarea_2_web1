c54a74f4606096fde58139e281ed5ddde6637cf8
RonaldP007 <paniaguarpg007@gmail.com>
2018-09-30 21:00:40 -0600

    Modificacion

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ if (($gestor = fopen("products.csv", "r")) !== FALSE) {
     while (($datos = fgetcsv($gestor, 1000, ",")) !== FALSE) {
         $numero = count($datos);
         $contador = 0;
-        $titulos =5;
         for ($c=0; $c < $numero; $c++) {
             echo $datos[$c];
             if($contador >4){
